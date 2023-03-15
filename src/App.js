d8dd7bc47d94e81a7fbb74f8eaf53d7255acb9d7
Ifeanyi Osi-Okeke <75077705+ifeanyiosi@users.noreply.github.com>
2023-03-15 22:48:09 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  // Navigate,
+  // Outlet,
+} from "react-router-dom";
+import routes from "./routes/routes.const";
+import LandingPage from "./pages/LandingPage";
+import Home from "./pages/Home";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<LandingPage />} />
+        <Route path={routes.home} element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/routes/routes.const.js
@@ -0,0 +1,23 @@
+const routes = {
+  // root: "/",
+  home: "/",
+  dashboard: "/dashboard",
+  profile: "/profile",
+  editProfile: "/profile/edit",
+  addBank: "/settings/bank-setup",
+  addBankOtp: "/settings/bank-setup/otp",
+  addBankPassword: "/settings/bank-setup/password",
+  addBankConfirmation: "/settings/bank-setup/confirmation",
+  addCard: "/settings/add-card",
+  addCardOtp: "/settings/add-card/otp",
+  addCardPassword: "/settings/add-card/password",
+  addCardConfirmation: "/settings/add-card/confirmation",
+  addBvn: "/settings/verify-bvn",
+  bvnVerifySuccess: "/settings/verify-bvn/success",
+  login: "/auth/login",
+  signup: "/auth/signup",
+  verify: "/auth/verify-account",
+  landingPage: "/welcome",
+};
+
+export default routes;
